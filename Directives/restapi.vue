033cc8228e321995810294ad0dbf410009ddf401
Brian Denicola <bjd145@yahoo.com>
2018-08-23 07:16:38 -0500

    New vue file

---
+++ Directives/restapi.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul v-if="posts && posts.length">
+    <li v-for="post of posts" :key = "post.title" >
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+  </ul>
+
+  <ul v-else-if="errors && errors.length">
+    <li v-for="error of errors" :key = "error.message" >
+      {{error.message}}
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+  created() {
+    axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
